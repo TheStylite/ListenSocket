3d2b3224eac40384ac11d3247202112a1bf48cfb
TS <33770747+TheStylite@users.noreply.github.com>
2019-06-10 14:59:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ socket.on('message',(e) => {
 
   Listener.deliver(JSON.parse(e.data));     //发布数据
       //这里要判断下用户验证是否成功
-  try {         //这里还要判断下当前页面是否在box系统，如果不在，就要捕获错误
+  try {         //这里还要判断下当前页面是否在主系统，如果不在，就要捕获错误
     if(e.data.code == 2){   //失败
         app.$router.push('/');      //返回重新登录
         socket.close();
